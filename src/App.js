df49d617f0a5f2a5770c5c925813d673a0e4b96c
SM-durofusic <d.fusic@searchmetrics.com>
2019-08-22 14:28:52 +0200

    Hopefully this will work

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const App = () => (
   <div>
     <h1 className={css({ color: 'red' })}>App</h1>
     <p>Continous integration is very fun!!!</p>
+    <p>This was built using CircleCI!!</p>
   </div>
 );
 
