14d6ed2ac964c76acb36b78dab22930d97280e93
SM-durofusic <d.fusic@searchmetrics.com>
2019-08-22 14:44:04 +0200

    HAPPY

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const App = () => (
     <h1 className={css({ color: 'red' })}>App</h1>
     <p>Continous integration is very fun!!!</p>
     <p>This was built using CircleCI!!</p>
+    <h1>I AM SO HAPPY</h1>
   </div>
 );
 
